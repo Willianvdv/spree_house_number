88358455e3b4253e0fe0ffd8337fdd40247fc0d2
Willian <willian@pythonheads.nl>
2012-11-16 17:21:58 +0100

    Replaced addresses with street and housenumber

--- app/overrides/add_house_number_to_address_form.rb
+++ app/overrides/add_house_number_to_address_form.rb
@@ -2,4 +2,4 @@ Deface::Override.new(
     :virtual_path => "spree/checkout/_address",
     :name => "add_house_number_to_address",
     :replace => "#baddress2",
-    :partial => 'spree/housenumber/_housenumber')
+    :partial => 'spree/checkout/housenumber')
--- app/overrides/add_street_to_address_form.rb
+++ app/overrides/add_street_to_address_form.rb
@@ -2,4 +2,4 @@ Deface::Override.new(
     :virtual_path => "spree/checkout/_address",
     :name => "add_street_to_address",
     :replace => "#baddress1",
-    :text => 'Street')
+    :partial => 'spree/checkout/street')
