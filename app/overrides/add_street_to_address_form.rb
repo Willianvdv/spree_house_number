7069de7b906d4ccf33fe75d56c99c632b23faae0
Rein Aris <info@reinaris.nl>
2012-11-22 16:11:56 +0100

    added shipping street

--- app/overrides/add_street_to_address_form.rb
+++ app/overrides/add_street_to_address_form.rb
@@ -1,5 +1,12 @@
 Deface::Override.new(
     :virtual_path => "spree/checkout/_address",
-    :name => "add_street_to_address",
+    :name => "add_street_to_billing_address",
     :replace => "#baddress1",
-    :partial => 'spree/checkout/street')
+    :partial => 'spree/checkout/street_billing')
+
+Deface::Override.new(
+    :virtual_path => "spree/checkout/_address",
+    :name => "add_street_to_shipping_address",
+    :replace => "#saddress1",
+    :partial => 'spree/checkout/street_shipping)
+
