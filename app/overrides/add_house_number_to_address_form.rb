975b4cc765fc3fe74029e3f559532b1b19c9293c
Rein Aris <info@reinaris.nl>
2012-11-22 15:56:12 +0100

    add3ed street number

--- app/overrides/add_house_number_to_address_form.rb
+++ app/overrides/add_house_number_to_address_form.rb
@@ -1,5 +1,11 @@
 Deface::Override.new(
     :virtual_path => "spree/checkout/_address",
-    :name => "add_house_number_to_address",
+    :name => "add_house_number_to_billing_address",
     :replace => "#baddress2",
+    :partial => 'spree/checkout/house_number')
+
+Deface::Override.new(
+    :virtual_path => "spree/checkout/_address",
+    :name => "add_house_number_to_shipping_address",
+    :replace => "#saddress2",
     :partial => 'spree/checkout/house_number')
